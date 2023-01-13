b652e8a06851f7ecf33018ce67fcc71267e7f834
FourLineCode <akmal3535.ah@gmail.com>
2023-01-13 19:56:37 +0600

    Laravel backend added

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -1,12 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
 };
